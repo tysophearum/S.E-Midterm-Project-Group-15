04de8af351e37c157dff91671bdceeb3b8e31804
Ty_Chhenglong <tychhenglong1@gmail.com>
2023-05-24 09:48:45 +0700

    login page

---
+++ src/main/resources/templates/Demo/Js_Table.js
@@ -0,0 +1,185 @@
+function myFunction() {
+    var x = document.querySelector(".detail1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction1() {
+    var x = document.querySelector(".detail2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction2() {
+    var x = document.querySelector(".detail3");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction3() {
+    var x = document.querySelector(".detail4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction4() {
+    var x = document.querySelector(".detail5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction5() {
+    var x = document.querySelector(".detail6");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction6() {
+    var x = document.querySelector(".detail7");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction7() {
+    var x = document.querySelector(".detail8");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction8() {
+    var x = document.querySelector(".detail9");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction9() {
+    var x = document.querySelector(".detail10");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction10() {
+    var x = document.querySelector(".detail11");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction11() {
+    var x = document.querySelector(".detail12");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction12() {
+    var x = document.querySelector(".detail13");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction13() {
+    var x = document.querySelector(".detail14");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction14() {
+    var x = document.querySelector(".detail15");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction15() {
+    var x = document.querySelector(".detail16");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction16() {
+    var x = document.querySelector(".detail17");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction17() {
+    var x = document.querySelector(".detail18");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction18() {
+    var x = document.querySelector(".detail19");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction19() {
+    var x = document.querySelector(".detail20");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction20() {
+    var x = document.querySelector(".detail21");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction21() {
+    var x = document.querySelector(".detail22");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction22() {
+    var x = document.querySelector(".detail23");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction23() {
+    var x = document.querySelector(".detail24");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}function myFunction24() {
+    var x = document.querySelector(".detail25");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
